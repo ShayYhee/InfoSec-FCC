e60a51047cc06386d15fb332b81277d8ba5ea0a1
ShayYhee <60741612+ShayYhee@users.noreply.github.com>
2023-09-22 23:07:58 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
 const helmet = require('helmet');
-const app = helmet();
+// const app = helmet();
 
 
 
